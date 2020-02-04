614862a01689943f94923a113ec4b560c706ecb5
Lakshit Paliwal <39520168+lakshit27@users.noreply.github.com>
2020-02-04 16:34:27 +0530

    for header
    by function
    and onClick={changeText} event

---
+++ Headermy.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+function Headermy (){
+    function changeme() {
+        console.log('hello form');
+    }
+    return <div className="header">
+             <h6>down hiiii lakshit paliwal</h6>
+             <p>down my super badboy</p>
+             <button onClick={changeme}>Header click here to change event</button>
+         </div>                
+    } 
+
+
+export default Headermy;
